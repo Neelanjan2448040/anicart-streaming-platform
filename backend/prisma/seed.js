c19e6b120343409268a5684dc04cb3ecb62730dc
unknown <neelanjandutta122@gmail.com>
2026-06-14 16:29:40 +0530

    chore: Update movie YouTube trailer embed links

--- backend/prisma/seed.js
+++ backend/prisma/seed.js
@@ -10,43 +10,43 @@ async function main() {
         title: "Attack on Titan",
         description: "After his hometown is destroyed and his mother is killed, young Eren Yeager vows to cleanse the earth of the giant humanoid Titans that have brought humanity to the brink of extinction.",
         image: "aot.png",
-        videoUrl: "https://www.youtube.com/embed/MGRm4IzK1SQ"
+        videoUrl: "https://www.youtube.com/embed/FU9-3Fmoh7w"
       },
       {
         title: "Naruto Shippuden",
         description: "Naruto Uzumaki is a loud, hyperactive ninja who constantly searches for approval and recognition.",
         image: "naruto.jpg",
-        videoUrl: "https://www.youtube.com/embed/-G9BqkgZXUk" 
+        videoUrl: "https://www.youtube.com/embed/QktQUO6gCAM" 
       },
       {
         title: "Dragon Ball Super",
         description: "Goku and his friends defend the Earth against an assortment of villains.",
         image: "dbs.png",
-        videoUrl: "https://www.youtube.com/embed/-EolXoH_eWA"
+        videoUrl: "https://www.youtube.com/embed/BhWjnTa03xs"
       },
       {
         title: "Your Name",
         description: "Two teenagers share a profound, magical connection upon discovering they are swapping bodies.",
         image: "yourname.jpg",
-        videoUrl: "https://www.youtube.com/embed/xU47nhruN-Q"
+        videoUrl: "https://www.youtube.com/embed/mcSwBhXGcLU"
       },
       {
         title: "Death Note",
         description: "A student goes on a secret crusade to eliminate criminals from the world after discovering a notebook capable of killing anyone.",
         image: "deathnote.png", 
-        videoUrl: "https://www.youtube.com/embed/NlJZ-YgSqao"
+        videoUrl: "https://www.youtube.com/embed/NlJZ-YgAt-c"
       },
       {
         title: "Demon Slayer",
         description: "A family is attacked by demons and only two members survive - Tanjiro and his sister Nezuko.",
         image: "demonslayer.png",
-        videoUrl: "https://www.youtube.com/embed/VQGCKyvncas"
+        videoUrl: "https://www.youtube.com/embed/MMe4ejk3zv4"
       },
       {
         title: "Chhota Bheem",
         description: "An adventurous and fun-loving boy, Bheem, along with his friends, protects his town of Dholakpur from evil forces and helps people in need.",
         image: "chhotabheem.png",
-        videoUrl: "https://www.youtube.com/embed/qF-u0Y6xTns"
+        videoUrl: "https://www.youtube.com/embed/0QrnZntffxA"
       }
     ]
   });
